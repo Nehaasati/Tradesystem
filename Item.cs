e9557f407406c441a27541bced03ff88c8a2a1a2
Nehaasati <31asatineha@gmail.com>
2025-09-27 23:57:42 +0200

    create user class and add private password

--- Item.cs
+++ Item.cs
@@ -1,1 +1,19 @@
 namespace App;
+public class Item
+{
+public string Name { get; set; }
+public string Description { get; set; }
+
+
+public Item(string name, string description)
+{
+Name = name;
+Description = description;
+}
+
+
+public override string ToString()
+{
+return $"Item: {Name}, Description: {Description}";
+}
+}
--- User.cs
+++ User.cs
@@ -1,3 +1,31 @@
 namespace App;
 
+public class User
+{
+    
+public string Username { get; private set; }
+private string Password;
+public List<Item> Items { get; private set; }
+
+
+public User(string username, string password)
+{
+Username = username;
+Password = password;
+Items = new List<Item>();
+}
+
+
+// Basic login method
+public bool Login(string username, string password)
+{
+return Username == username && Password == password;
+}
+
+
+public void AddItem(Item item)
+{
+Items.Add(item);
+}
+}
 
